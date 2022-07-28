310accad78cf380d22ffd04428c7b1c32d3ba2c5
Diana <66874874+silverlunged@users.noreply.github.com>
2022-07-28 13:25:40 +0300

    Added functionality regarding wedding ring(marry_kitty() and extra points)

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Backpack:
 
 
 def mainloop(backpack):
+    # main loop. User chooses which way to go
     user_input = ''
     while user_input != 'q' and backpack.lives != 0:
         user_input = input("---------------\nType 'q' to quit\nType 't' to dig in the trash\nType 'r' to visit the roof\nType 'p' to see your phonebook\n>")
@@ -21,16 +22,21 @@ def mainloop(backpack):
         elif user_input == 't':
             dig_in_the_trash(backpack)
         elif user_input == 'p':
-            for kitty in backpack.phonebook:
-                print("-----")
-                print(kitty.title())
-                print("Breed: " + kitties[kitty][0])
-                print("Description: " + kitties[kitty][1])
-                print("Treasure: " + kitties[kitty][2])
-                print("-----")
+            print_phonebook(backpack.phonebook)
+
+
+def print_phonebook(phonebook):
+    for kitty in phonebook:
+        print("-----")
+        print(kitty.title())
+        print("Breed: " + kitties[kitty]['breed'])
+        print("Personality: " + kitties[kitty]['personality'])
+        print("Treasure: " + kitties[kitty]['treasure'])
+        print("-----")
 
 
 def homescreen():
+    # greeting the user, creating a backpack
     print("--------------------------------\nWelcome to Trash Kitty Love!")
     print(r"""
         |\__/,|   (`\
@@ -67,6 +73,16 @@ def dig_in_the_trash(backpack):
         print("---GAME OVER---")
         print(f"Your score is {backpack.points} points")
         homescreen()
+    elif choice == the_treasure == "wedding ring":
+        if not backpack.phonebook:
+            print("You found a wedding ring, but you've got no one to propose to. Take 50 points, you loveless poor baby.")
+            backpack.points += 50
+        user_input = ('You found a wedding ring. You must now make a choice between love and money. Take 50 points and walk away (g) or propose to one of the kitties from your phonebook (p)? ')
+        if user_input == 'g':
+            backpack.points += 50
+            print("Congrats! 50 points is yours.")
+        elif user_input == 'p':
+            marry_kitty(backpack.phonebook)
     elif choice == the_treasure and the_treasure in backpack.treasures:
         print(f"You already have {the_treasure} in your backpack!")
     elif choice == the_treasure:
@@ -90,13 +106,22 @@ def meet_kitty(backpack):
     elif not backpack.treasures:
         print(f"Kitties dig those who dig in the trash. You met {kitty.title()}, but you have nothing to offer.")
     elif kitty not in backpack.phonebook:
-        user_input = input(f"You met {kitty.title()}. Offer them one of your treasures: {backpack.treasures}\nHere's a hint: {kitties[kitty]['hint']}>")
-        if user_input == kitties[kitty][2]:
+        user_input = input(f"You met {kitty.title()}. Offer them one of your treasures: {backpack.treasures}\nHere's a hint: {kitties[kitty]['hint']}\n>")
+        if user_input == kitties[kitty]['treasure']:
             backpack.phonebook.append(kitty)
             backpack.treasures.remove(user_input)
             print(f"{kitty.title()} gave you their number. Call them sometime ;)")
         else:
             print(f"{kitty.title()} is disappointed. They don't like things such as {user_input}.")
+
+
+def marry_kitty(phonebook):
+    print("Who do choose to marry?")
+    print_phonebook(phonebook)
+    user_input = input('>').lower()
+    if user_input in phonebook and user_input in kitties:
+        print("Here's what the future holds for you: ")
+        print(kitties[user_input]['future'])
 
 
 homescreen()
